dc8033c18515062fedec4062365f57e2ef7043bf
teoofrast <teoofrast@gmaill.com>
2024-02-19 11:48:43 +0600

    add config, database, models, schemas

---
+++ src/database.py
@@ -0,0 +1,19 @@
+import asyncio
+from typing import Annotated
+from sqlalchemy import String, create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
+
+from config import settings
+
+sync_engine = create_engine(
+    url=settings.DATABASE_URL_psycopg,
+    echo=True,
+    # pool_size=5,
+    # max_overflow=10,
+)
+
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg,
+    echo=True,
+)
