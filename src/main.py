abe0fc03e251d58d2bd7ad3fbd562f359a5d8289
teoofrast <63227600+teoofrast@users.noreply.github.com>
2024-02-22 13:43:47 +0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,12 +5,3 @@ from queries.core import SyncCore
 from queries.orm import SyncOrm
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-
-
-# SyncOrm.create_tables()
-# SyncOrm.insert_workers()
-# SyncCore.select_workers()
-# SyncCore.update_worker()
-
-
-SyncOrm.select_resumes_avg_compensation()
